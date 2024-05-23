6658f4516603446cc8736dea35f8da35e0854f6e
nahidraz23 <nzamanraz@gmail.com>
2024-05-23 18:33:06 +0600

    Our menu page is updated

--- src/Components/shared/ItemCard.jsx
+++ src/Components/shared/ItemCard.jsx
@@ -6,11 +6,11 @@ const ItemCard = ({ item }) => {
                 <div>
                     <img src={image} alt="" style={{ width: "120px", borderRadius: '0 200px 200px 200px' }} />
                 </div>
-                <div className="space-y-2">
-                    <h2 className="text-xl uppercase">{name} ---------------------------------------------------------</h2>
+                <div className="flex flex-col justify-center w-full space-y-2">
+                    <h2 className="text-xl uppercase">{name} ------------------------</h2>
                     <h2>{recipe}</h2>
                 </div>
-                <div>
+                <div className="flex w-1/2 justify-end">
                     <p className="text-yellow-600 text-lg">${price}</p>
                 </div>
         </div>
