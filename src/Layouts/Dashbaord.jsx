c209134d4d8af4e4a649de1325910b38d35a2f2f
nahidraz23 <nzamanraz@gmail.com>
2024-05-27 03:34:53 +0600

    My cart implented in dashboard  page

--- src/Layouts/Dashbaord.jsx
+++ src/Layouts/Dashbaord.jsx
@@ -1,12 +1,12 @@
-import { FaCalendar, FaCalendarDay, FaCartPlus, FaComment, FaHome, FaStreetView, FaWallet } from "react-icons/fa";
+import { FaCalendar, FaCalendarDay, FaCartPlus, FaComment, FaEnvelope, FaHome, FaList, FaMailBulk, FaShoppingBag, FaStreetView, FaWallet } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashbaord = () => {
     return (
         <div className="grid grid-cols-12">
-            <div className="grid col-span-3 bg-orange-400 min-h-screen">
+            <div className="grid col-span-3 bg-black text-white min-h-screen p-5">
                 <ul>
-                    <NavLink>
+                    <NavLink to={'/dashboard/userHome'}>
                         <li className="flex gap-2 p-4 items-center text-2xl hover:bg-white">
                             <FaHome></FaHome>
                             User Home
@@ -24,7 +24,7 @@ const Dashbaord = () => {
                             Payment History
                         </li>
                     </NavLink>
-                    <NavLink to={'/dashboard/mycart'}>
+                    <NavLink to={'/dashboard/mycart'} className={(isActive) => isActive ? 'text-white' : 'text-black'}>
                         <li className="flex gap-2 p-4 items-center text-2xl hover:bg-white">
                             <FaCartPlus></FaCartPlus>
                             My Cart
@@ -42,9 +42,36 @@ const Dashbaord = () => {
                             My Booking
                         </li>
                     </NavLink>
+
+                    <hr className="my-5"/>
+
+                    <NavLink to={'/'}>
+                        <li className="flex gap-2 p-4 items-center text-2xl hover:bg-white">
+                            <FaHome></FaHome>
+                            Home
+                        </li>
+                    </NavLink>
+                    <NavLink to={'/menu'}>
+                        <li className="flex gap-2 p-4 items-center text-2xl hover:bg-white">
+                            <FaList></FaList>
+                            Menu
+                        </li>
+                    </NavLink>
+                    <NavLink>
+                        <li className="flex gap-2 p-4 items-center text-2xl hover:bg-white">
+                            <FaShoppingBag></FaShoppingBag>
+                            Shop
+                        </li>
+                    </NavLink>
+                    <NavLink>
+                        <li className="flex gap-2 p-4 items-center text-2xl hover:bg-white">
+                            <FaEnvelope></FaEnvelope>
+                            Contact
+                        </li>
+                    </NavLink>
                 </ul>
             </div>
-            <div className="grid col-span-9 bg-[#F6DCAC]">
+            <div className="grid col-span-9 bg-gray-700">
                 <div className="text-center">
                     <Outlet></Outlet>
                 </div>
--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -1,7 +1,76 @@
+import { FaTrash } from "react-icons/fa";
+import SectionHeading from "../../../Components/shared/SectionHeading";
+import useCart from "../../../hooks/useCart";
+
 const MyCart = () => {
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((sum, item) => sum + item.price, 0)
+
     return (
         <div>
-            Welcome to my cart
+            <div>
+                <SectionHeading
+                    subHeading={'My Cart'}
+                    heading={'Wanna Add More?'}
+                ></SectionHeading>
+            </div>
+            <div>
+                <div className="flex justify-evenly items-center">
+                    <h1 className="text-3xl font-semibold uppercase">Total Orders: {cart.length}</h1>
+                    <h1 className="text-3xl font-semibold uppercase">Total Price: ${totalPrice}</h1>
+                    <button className="btn bg-orange-400 text-white">Pay</button>
+                </div>
+                <div className="p-5 my-5">
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            {/* head */}
+                            <thead className="bg-orange-600 rounded-t-xl">
+                                <tr>
+                                    <th>
+
+                                    </th>
+                                    <th>Item Image</th>
+                                    <th>Item Name</th>
+                                    <th>Item Price</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    cart.map(item =>
+                                        <tr key={item._id}>
+                                            <td>
+
+                                            </td>
+                                            <td>
+                                                <div className="avatar">
+                                                    <div className="mask mask-squircle w-12 h-12">
+                                                        <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div>
+                                                    <div className="font-bold">{item.name}</div>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div>{item.price}</div>
+                                            </td>
+                                            <td>
+                                                <button className="btn">
+                                                    <FaTrash></FaTrash>
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
